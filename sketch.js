879631a5eb212720beddf72854ea122ed71fada0
Adam Azmi <125698188+Adamazmi-LSU@users.noreply.github.com>
2025-02-12 00:22:26 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,69 +1,56 @@
-let guy;
-let green;
-let lime;
-let character;
+let guy, green, lime;
+let characters = [];
 
 function preload() {
   guy = loadImage("media/SpelunkyGuy.png");
   green = loadImage("media/Green.png");
-  lime = loadImage("media/Lime.png")
+  lime = loadImage("media/Lime.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   imageMode(CENTER);
 
-  character = new Character(random(80, width-80),random(80, height-80));
-  character.addAnimation("down", new SpriteAnimation(guy, 6, 5, 6));
-  character.addAnimation("up", new SpriteAnimation(guy, 0, 5, 6));
-  character.addAnimation("stand", new SpriteAnimation(guy, 0, 0, 1));
-  character.addAnimation("right" , new SpriteAnimation(guy, 0, 9, 4));
-  character.addAnimation("left" , new SpriteAnimation(guy, 0, 9, 4));
-  character.currentAnimation = "stand";
-  character2 = new Character(random(80, width-80),random(80, height-80));
-  character2.addAnimation("down", new SpriteAnimation(green, 6, 5, 6));
-  character2.addAnimation("up", new SpriteAnimation(green, 0, 5, 6));
-  character2.addAnimation("stand", new SpriteAnimation(green, 0, 0, 1));
-  character2.addAnimation("right" , new SpriteAnimation(green, 0, 9, 4));
-  character2.addAnimation("left" , new SpriteAnimation(green, 0, 9, 4));
-  character2.currentAnimation = "stand";
-  character3 = new Character(random(80, width-80),random(80, height-80));
-  character3.addAnimation("down", new SpriteAnimation(lime, 6, 5, 6));
-  character3.addAnimation("up", new SpriteAnimation(lime, 0, 5, 6));
-  character3.addAnimation("stand", new SpriteAnimation(lime, 0, 0, 1));
-  character3.addAnimation("right" , new SpriteAnimation(lime, 0, 9, 4));
-  character3.addAnimation("left" , new SpriteAnimation(lime, 0, 9, 4));
-  character3.currentAnimation = "stand";
-  
+  // Creating character instances and storing them in an array
+  characters.push(new Character(random(80, width - 80), random(80, height - 80), guy));
+  characters.push(new Character(random(80, width - 80), random(80, height - 80), green));
+  characters.push(new Character(random(80, width - 80), random(80, height - 80), lime));
 }
 
 function draw() {
   background(220);
 
-  character.draw();
-  character2.draw();
-  character3.draw();
-
+  // Draw each character
+  for (let character of characters) {
+    character.draw();
+  }
 }
 
 function keyPressed() {
-  character.keyPressed();
-  character2.keyPressed();
-  character3.keyPressed();
+  for (let character of characters) {
+    character.keyPressed();
+  }
 }
 
 function keyReleased() {
-  character.keyReleased();
-  character2.keyReleased();
-  character3.keyReleased();
+  for (let character of characters) {
+    character.keyReleased();
+  }
 }
 
 class Character {
-  constructor(x, y) {
+  constructor(x, y, spriteSheet) {
     this.x = x;
     this.y = y;
-    this.currentAnimation = null;
+    this.currentAnimation = "stand";
     this.animations = {};
+    this.spriteSheet = spriteSheet;
+
+    this.addAnimation("down", new SpriteAnimation(spriteSheet, 6, 5, 6));
+    this.addAnimation("up", new SpriteAnimation(spriteSheet, 0, 5, 6));
+    this.addAnimation("stand", new SpriteAnimation(spriteSheet, 0, 0, 1));
+    this.addAnimation("right", new SpriteAnimation(spriteSheet, 0, 9, 4));
+    this.addAnimation("left", new SpriteAnimation(spriteSheet, 0, 9, 4));
   }
 
   addAnimation(key, animation) {
@@ -77,7 +64,7 @@ class Character {
         case "up":
           this.y -= 2;
           break;
-        case "down": 
+        case "down":
           this.y += 2;
           break;
         case "right":
@@ -95,7 +82,7 @@ class Character {
   }
 
   keyPressed() {
-    switch(keyCode) {
+    switch (keyCode) {
       case UP_ARROW:
         this.currentAnimation = "up";
         break;
@@ -111,17 +98,15 @@ class Character {
         break;
     }
   }
-  
+
   keyReleased() {
-    if (keyCode === LEFT_ARROW){
+    if (keyCode === LEFT_ARROW) {
       this.currentAnimation = "stand";
       this.animations["stand"].flipped = true;
     } else {
       this.currentAnimation = "stand";
       this.animations["stand"].flipped = false;
-    } 
-    
-    //this.animations[this.currentAnimation].flipped = true;
+    }
   }
 }
 
@@ -137,16 +122,13 @@ class SpriteAnimation {
   }
 
   draw() {
-
-    let s = (this.flipped) ? -1 : 1;
-    scale(s,1);
-    image(this.spritesheet, 0, 0, 80, 80, this.u*80, this.v*80, 80, 80);
+    let s = this.flipped ? -1 : 1;
+    scale(s, 1);
+    image(this.spritesheet, 0, 0, 80, 80, this.u * 80, this.v * 80, 80, 80);
 
     this.frameCount++;
-    if (this.frameCount % 10 === 0)
-      this.u++;
+    if (this.frameCount % 10 === 0) this.u++;
 
-    if (this.u === this.startU + this.duration)
-      this.u = this.startU;
+    if (this.u === this.startU + this.duration) this.u = this.startU;
   }
 }
